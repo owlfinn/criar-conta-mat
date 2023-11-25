f37c6e661052266c92c6c4ece1b0a83d0c0903dd
owlfinn <106352997+owlfinn@users.noreply.github.com>
2023-11-25 12:42:34 -0300

    Add files via upload

---
+++ resolverproblema_mat.py
@@ -0,0 +1,28 @@
+import random
+
+e = 0
+user_replay = int(input(f'Quantas vezes você quer jogar?: '))
+def gerarproblema():
+    op = ["+", "-", "*"]
+    op = random.choice(op)
+
+    p1 = random.randint(1, 10)
+    p2 = random.randint(1, 10)
+
+    ex = f'{p1} {op} {p2}'
+    r = eval(ex)
+    return ex, r
+
+for i in range(user_replay):
+    ex, r = gerarproblema()
+    user_r = int(input(f'PROBLEMA {i+1}: {ex} ({r}) '))
+    
+    while True:
+        if user_r == r:
+            break
+        else:
+            e += 1
+            print("e")
+            break
+
+print(f'O seu total de acertos foi: {user_replay - e}')
